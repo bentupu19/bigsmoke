a558a95f02978df4a57eefc4fbbc8f6195775a3f
bentupu19 <bentupu433@gmail.com>
2025-06-25 01:10:35 +0800

    Create category.js

---
+++ category.js
@@ -0,0 +1,19 @@
+module.exports = {
+  categories: {
+    umum: [
+      {
+        id: 1,
+        name: "Diamond ML 86",
+        price: 20000,
+        desc: "Topup Mobile Legends 86DM",
+      },
+      {
+        id: 2,
+        name: "Pulsa 10k",
+        price: 12000,
+        desc: "Pulsa Telkomsel/Indosat/XL",
+      },
+    ],
+  },
+  lastId: 2
+};
